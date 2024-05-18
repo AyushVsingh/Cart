ca1755c04a3f318eb29b52f8de9357e3c15fcaa8
unknown <aayushsingh050801@gmail.com>
2024-05-18 13:34:44 +0530

    Final commit

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,52 +1,46 @@
 import React from 'react';
 
-class CartItem extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
-    }
-    increaseQuantity = () => {
-        console.log('this', this.state);
-    }
-    render() {
-        const { price, title, qty, img } = this.state;
-        return (
-            <div className='cart-item'>
-                <div className='left-block'>
-                    <img style={styles.image} />
-                </div>
-                <div className='right-block'>
-                    <div style={{ fontSize: 25 }}>{title}</div>
-                    <div style={{ color: '#777' }}>Rs {price}</div>
-                    <div style={{ color: '#777' }}>Qty:{qty}</div>
-                    <div className='cart-item-actions'>
-                        <img
-                            alt="increase"
-                            className='action-icons'
-                            src="https://www.svgrepo.com/show/522230/plus-circle.svg"
-                            onClick={this.increaseQuantity}
-                        />
-                        <img
-                            alt="decrease"
-                            className='action-icons'
-                            src="https://www.svgrepo.com/show/522961/minus-circle.svg"
-                        />
-                        <img
-                            alt="delete"
-                            className='action-icons'
-                            src="https://www.svgrepo.com/show/511788/delete-1487.svg"
-                        />
-                    </div>
+const CartItem = (props) => {
+    const { price, title, qty } = props.product;
+    const {
+        product,
+        onIncreaseQuantity,
+        onDecreaseQuantity,
+        onDeleteProduct
+    } = props;
+    return (
+        <div className="cart-item">
+            <div className="left-block">
+                <img style={styles.image} src={product.img} />
+            </div>
+            <div className="right-block">
+                <div style={{ fontSize: 25 }}>{title}</div>
+                <div style={{ color: '#777' }}>Rs {price} </div>
+                <div style={{ color: '#777' }}>Qty: {qty} </div>
+                <div className="cart-item-actions">
+                    {/* Buttons */}
+                    <img
+                        alt="increase"
+                        className="action-icons"
+                        src="https://www.svgrepo.com/show/509388/plus-circle.svg"
+                        onClick={() => onIncreaseQuantity(product)}
+                    />
+                    <img
+                        alt="decrease"
+                        className="action-icons"
+                        src="https://www.svgrepo.com/show/512492/minus-circle-1429.svg"
+                        onClick={() => onDecreaseQuantity(product)}
+                    />
+                    <img
+                        alt="delete"
+                        className="action-icons"
+                        src="https://www.svgrepo.com/show/502614/delete.svg"
+                        onClick={() => onDeleteProduct(product.id)}
+                    />
                 </div>
             </div>
-        )
-    }
+        </div>
+    );
 }
 
 const styles = {
